7b7567fea25e8077863931fefd7c2bb042d77556
Thapelo <thapeloletsapa@gmail.com>
2024-05-30 16:50:51 +0200

    "Removed form tags, added import statement, and reformatted input fields"

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,29 +1,26 @@
+import memesData from "./memesData"
 
 export default function Meme(){
+
     return (
         
         <main>
-            <form className="form">
-                <div> 
-                    <label>Top Text
+            <div className="form">               
+                     
                         <input 
                         type="text" 
                         className="form--input" 
-                        placeholder="Shut up"/>
-                    </label>
-                </div>
-                <div> 
-                    <label>Bottom Text
+                        placeholder="Shut up"
+                        />           
                         <input 
                         type="text" 
                         className="form--input" 
-                        placeholder="and take my money"/>
-                    </label>   
-                </div>
-                <button className="form--button">
+                        placeholder="and take my money"
+                        />       
+                <button  className="form--button">
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
         </main>
 
     )
