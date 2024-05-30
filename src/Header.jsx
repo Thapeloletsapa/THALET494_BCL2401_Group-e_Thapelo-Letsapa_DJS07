dfc330e57d28afb94535e8a71eb7b5122cfe2720
Thapelo <thapeloletsapa@gmail.com>
2024-05-30 15:42:57 +0200

    "Updated Header component to return a header element with image and multiple headings instead of a single h1 element."

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,10 @@
 
 
 export default function Header(){
-    return <h1>Hello World</h1>
+    return (
+    <header>
+     <img src="../public/images/troll-face.png"/> 
+     <h2>Header component</h2>
+     <h4>React Course - Project 3</h4>
+    </header>)
 }
