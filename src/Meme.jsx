56980b5ff6859bd417a9e9b541b641b2dfd7f323
Thapelo <thapeloletsapa@gmail.com>
2024-05-30 16:29:33 +0200

    "Updated form inputs in Meme component to include labels and revised placeholders"

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -4,8 +4,22 @@ export default function Meme(){
         
         <main>
             <form className="form">
-                <input type="text" className="form--input" placeholder="Top text"/>
-                <input type="text" className="form--input" placeholder="Bottom text"/>
+                <div> 
+                    <label>Top Text
+                        <input 
+                        type="text" 
+                        className="form--input" 
+                        placeholder="Shut up"/>
+                    </label>
+                </div>
+                <div> 
+                    <label>Bottom Text
+                        <input 
+                        type="text" 
+                        className="form--input" 
+                        placeholder="and take my money"/>
+                    </label>   
+                </div>
                 <button className="form--button">
                     Get a new meme image 🖼
                 </button>
